71ac7e9d3ab353720949421c27f34ebec0cc7174
murdererbrain <mehrand77@gmail.com>
2023-07-30 14:55:49 +0330

    initial commt

--- toodolist.js
+++ toodolist.js
@@ -11,47 +11,59 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 app.use(express.static('public'));
 app.use(express.json());
 
-app.get('/api/todolist', (req, res) => {
-    todoo.find()
-        .then((result) => res.send(result))
-        .catch((err) => console.log(err));
+app.get('/api/todolist', async (req, res) => {
+    try {const todooo= await todoo.find()
+        res.status(200).send(todooo)
+    } catch (e) {
+        res.status(500).send({'error':e.message})
+    }
+    
 });
 
 app.get('/api/todolist/:id', async (req, res) => {
     const id = req.params.id;
     try {
-        const todo = await todoo.findById(id)
-        res.status(200).send(todo)
+        const todo1 = await todoo.findById(id)
+        res.status(200).send(todo1)
     } catch (e) {
-        res.status(500).send({"error": e.message})
+        res.status(500).send({ "error": e.message })
     }
 });
 
-app.post('/api/todolist/', (req, res) => {
+app.post('/api/todolist/', async (req, res) => {
     const todo = new todoo({
         title: req.body.title,
         completed: false
     });
-
-    todo.save()
-        .then((result) => res.status(201).send(result))
-        .catch((err) => console.log(err));
+    try {
+        const todo2 = await todo.save()
+        res.status(201).send(todo2)
+    } catch (e) {
+        res.status(500).send({ 'error': e.message })
+    }
 });
 
 
-app.put('/api/todolist/:id', (req, res) => {
+app.put('/api/todolist/:id', async (req, res) => {
     const id = req.params.id;
     const title = req.body.title;
     const completed = req.body.completed;
 
-    todoo.findByIdAndUpdate(id, { title, completed })
-        .then((result) => res.status(202).send(result))
-        .catch((err) => console.log(err));
+    try {
+        const todo3 = await todoo.findByIdAndUpdate(id, { title, completed })
+        res.status(202).send(todo3)
+    } catch (e) {
+        res.status(500).send({ 'error ': e.message })
+    }
+
 });
 
-app.delete('/api/todolist/:id', (req, res) => {
+app.delete('/api/todolist/:id', async (req, res) => {
     const id = req.params.id;
-    todoo.findByIdAndDelete(id)
-        .then((result) => res.status(204))
-        .catch((err) => console.log(err));
-}); 
+    try {
+        const todo4 = await todoo.findByIdAndDelete(id)
+        res.status(204).send(todo4)
+    } catch (e) {
+        res.status(500).send({ 'Error': e.message })
+    }
+});
