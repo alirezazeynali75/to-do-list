759001ee610efc1ef37d5a13565abfeee097eaa9
murdererbrain <mehrand77@gmail.com>
2023-07-30 10:44:41 +0330

    initial committ

--- schema.js
+++ schema.js
@@ -4,7 +4,10 @@ const todolistschema = new Schema({
     title: {
         type: String,
         required: true,
-    }
+    },completed: {
+        type: Boolean,
+        default: false
+      }
 });
 const todo = mongoose.model('todo', todolistschema);
 module.exports = todo;
