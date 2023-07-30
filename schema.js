e6070c21a3dbb6972374894acbb8f1778ac3a884
murdererbrain <mehrand77@gmail.com>
2023-07-30 10:39:24 +0330

    initial committ

---
+++ schema.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const todolistschema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    }});
+const todo = mongoose.model('todo',todolistschema);
+module.exports = todo;
